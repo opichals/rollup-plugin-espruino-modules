3ad89d53601a921169070d015d66ccae8ba106c0
Standa Opichal <opichals@gmail.com>
2018-11-25 22:19:23 +0100

    feat(minify): support custom options

--- index.js
+++ index.js
@@ -41,32 +41,41 @@ const defaultMinifyOptions = { // -- terser, Espruino compatible options --
         // , mangle: false, compress: false
     };
 
+const buildMinifyConfig = (options) => {
+    const opts = {
+        ...defaultMinifyOptions,
+        ...options
+    };
+    if (typeof options.mangle === 'object') {
+        opts.mangle = { ...defaultMinifyOptions.mangle, ...options.mangle };
+    }
+    if (typeof options.compress === 'object') {
+        opts.compress = { ...defaultMinifyOptions.compress, ...options.compress };
+    }
+    return opts;
+};
+
 const buildPlugins = (options) => [
     gitHubModules(),
     espruinoModules(options.espruino),
     json(),
     commonjs(),
-    options.espruino.minify === false ? { requireId: () => null } : terser.plugin(defaultMinifyOptions),
+    options.espruino.minify === false
+        ? { requireId: () => null }
+        : terser.plugin(buildMinifyConfig(options.espruino.minify)),
 ];
 
-const buildRollupConfig = (baseOptions) => {
-    const opts = Object.assign({},
-        defaultOptions,
-        baseOptions,
-        { output: { ...defaultOptions.output, ...baseOptions.output } }
-    );
+const buildRollupConfig = (options) => {
+    const opts = {
+        ...defaultOptions,
+        ...options
+    };
+    opts.output = { ...defaultOptions.output, ...options.output };
     opts.espruino = opts.espruino || {};
     opts.plugins = buildPlugins(opts);
     delete opts.espruino;
     return opts;
 };
-
-const buildMinifyConfig = (options) => {
-    return {
-        ...defaultMinifyOptions,
-        ...options
-    };
-}
 
 module.exports = {
     espruinoModules,
